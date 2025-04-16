baf28ae1a1d8fa517271437e4b86323fe513b7cd
Tony <97451137+TonybynMp4@users.noreply.github.com>
2025-04-16 21:56:31 +0200

    feat: LandingPage & NotFoundPage (Chahd)

---
+++ client/src/pages/LandingPage.vue
@@ -0,0 +1,41 @@
+<template>
+    <main>
+		<section>
+			<h2>YFlop</h2>
+			<p>
+				Bienvenue sur YFlop, le réseau social préféré des gens toxiques !
+			</p>
+			<div class="actions">
+				<router-link to="/login">Se connecter</router-link>
+				<router-link to="/register">S'inscrire</router-link>
+			</div>
+		</section>
+    </main>
+</template>
+
+<style scoped>
+    main {
+        display: flex;
+		flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        height: 80vh;
+    }
+
+	.actions {
+		display: flex;
+		justify-content: center;
+		gap: 1rem;
+		margin-top: 2rem;
+	}
+
+	section {
+		transform: translateY(-100%);
+	}
+
+	h2 {
+		font-size: 2.5rem;
+		margin-bottom: 1rem;
+		font-weight: bolder;
+	}
+</style>
